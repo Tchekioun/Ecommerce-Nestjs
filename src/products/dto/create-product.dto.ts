09dc2a2e2adcc80510ad0d6423621230e84ba5b0
Abderraouf <abdelraouffinch@gmail.com>
2023-01-13 22:01:31 +0100

    edit controller

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,12 @@
-export class CreateProductDto {}
+export class CreateProductDto {
+  sku?: string;
+  name?: string;
+  description?: string;
+  unit_price?: number;
+  image_url?: string;
+  active?: number;
+  units_in_stock?: number;
+  date_created?: Date;
+  last_updated?: Date;
+  category_id?: number;
+}
