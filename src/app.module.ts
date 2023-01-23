7bf7bbd7feb34411d101cac568f2dc92d1942599
Abderraouf <abdelraouffinch@gmail.com>
2023-01-23 19:44:38 +0100

    generated order resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ProductsModule } from './products/products.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { CategoriesModule } from './categories/categories.module';
 import { CountriesModule } from './countries/countries.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [ProductsModule, PrismaModule, CategoriesModule, CountriesModule],
+  imports: [ProductsModule, PrismaModule, CategoriesModule, CountriesModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
